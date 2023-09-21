c0676e1eb65c2e263b169c45ad4c6bdd3189edb5
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-21 08:47:44 +0000

    learnt variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const emp_Id = 101
+let emp_Email = "john@gmail.com"
+var emp_Password = 12121212
+emp_City = "Lucknow"
+
+//emp_Id  = 102 //not allowed to re-assigned a value to const variable 
+
+emp_Email = "johnDear@gmaol.com"
+emp_Password = 13131313
+emp_City = "Delhi"
+
+/*
+Perefer not use to var 
+because of issue in block scope and functional scope
+*/
+
+console.log(emp_Id)
+console.table([emp_Id, emp_Email, emp_Password, emp_City])
+
