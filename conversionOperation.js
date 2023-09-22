49788b45b6a69b7165b551bc24fa888938dc5e17
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-22 08:30:25 +0000

    updated

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+let score = "Bittu"
+let valueInNumber = number(score)
+console.log(typeof valueInNumber)
+
+let anyNumber = 24
+
+let stringNumber = string(anyNumber)
+console.log(typeof stringNumber)
