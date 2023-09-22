4009409fb179d7c99fce697c6f285a0f323b05dd
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-22 09:38:14 +0000

    file moved to main

--- conversionOperation.js
+++
@@ -1,8 +0,0 @@
-let score = "Bittu"
-let valueInNumber = number(score)
-console.log(typeof valueInNumber)
-
-let anyNumber = 24
-
-let stringNumber = string(anyNumber)
-console.log(typeof stringNumber)
