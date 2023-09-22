85fe10c43c9e616832d719f0eaa7f4200afa303c
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-22 10:56:32 +0000

    datatypesSummary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,16 @@
+/* 
+Primitive datatypes - 7 types mainly : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ 
+ Reference (non-premitive) - Array, Objects, Functions
+
+ */
+const id = Symbol(123)
+const another_Id = Symbol(123)
+console.log(id === another_Id)
+
+//array, object
+
+const heros = [{name:"IronMan",
+ name2:"Shaktiman", name3:"Naagraj"}]
+ console.log(typeof heros);
