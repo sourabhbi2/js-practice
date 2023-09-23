3dafd38a1371857f9cf856a08dec6f0fab983822
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-23 09:19:56 +0000

    updated

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -8,9 +8,16 @@ Primitive datatypes - 7 types mainly : String, Number, Boolean, null, undefined,
 const id = Symbol(123)
 const another_Id = Symbol(123)
 console.log(id === another_Id)
+console.log(typeof another_Id)
 
 //array, object
 
 const heros = [{name:"IronMan",
  name2:"Shaktiman", name3:"Naagraj"}]
  console.log(typeof heros);
+
+
+ //====================================
+
+// Stack memory -> In which  Premitive datatypes stored 
+//Heap memory -> In which  non-premitive datatypes stored
