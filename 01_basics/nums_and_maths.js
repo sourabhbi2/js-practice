387f55fbbcd7eb235ac299b1aa095ee923b3cfff
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-24 08:29:21 +0000

    numsAndmaths

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const otherNum = 101.3334
+
+// console.log(otherNum.toPrecision(4));
+
+const num1 = 100000
+
+// console.log(num1.toLocaleString('en-In'));
+
+//==============Maths====================
+
+
+
+
+
+
