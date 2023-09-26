62b55b827b1915bc3304dcbd9e9f7f62a78c033c
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-09-26 07:06:44 +0000

    Math updated

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -1,7 +1,7 @@
 const score = 400
-console.log(score);
+// console.log(score);
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
 // console.log(balance.toString().length)
 // console.log(balance.toFixed(2));
@@ -16,6 +16,14 @@ const num1 = 100000
 
 //==============Maths====================
 
+// console.log(Math);
+// console.log(Math.abs(-9)); // abs means absolute value which means it change only negative value into positive
+// console.log(Math.round(4.9)) // for round off
+// console.log(Math.ceil(42.5)); // takes higher value i.e 43
+// console.log(Math.ceil(42.1)); // takes higher value even 0.1 value increases it gives 43
+// console.log(Math.floor(42.1)); // takes lower value
+
+console.log(Math.floor(Math.random() * 30)); // random() gives values in between 0 to 1 randomly
 
 
 
