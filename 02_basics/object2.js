3ad7e86d581c3f8b0d26fde0f2be9dc1854f5f2d
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-10-02 09:15:23 +0000

    learnt json

--- 02_basics/object2.js
+++ 02_basics/object2.js
@@ -31,12 +31,12 @@ const obj2 = {3: "c", 4: "d"}
 const obj = {...obj1, ...obj2}
 // console.log(obj);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser)); // returns string
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser)); //Returns an array of key/values of the enumerable properties of an object
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser)); // returns string
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser)); //Returns an array of key/values of the enumerable properties of an object
 
-console.log(tinderUser.hasOwnProperty('isLoggedOut'));
+// console.log(tinderUser.hasOwnProperty('isLoggedOut'));
 
 const course = {
     courseName: "Basics of JS",
@@ -44,3 +44,21 @@ const course = {
     courseInstructor: "Hitesh Chaudhary"
 }
 
+const {courseInstructor: instrustor} = course
+
+console.log(instrustor);
+
+/*json
+
+1. {
+   "name": "Durga",
+   "courseName": "Js Basics",
+   "price": "Free"
+}
+2. [
+    {},
+    {}
+] 
+json file can be in two formats 1. in object form and 2. in array form.
+
+*/
