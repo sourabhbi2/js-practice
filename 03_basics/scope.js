824caa6ca7fc3ab0c38f550a1e07a47b3455a9ec
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-10-03 07:02:46 +0000

    scope-2

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -4,10 +4,22 @@ let a = 300; // global scope
 if(true){
     let a = 10; // block scope
     const b = 20;
-    console.log('Inner a:', a);
+    // console.log('Inner a:', a);
     
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+console.log(addOne(9))
+function addOne(num){
+    return num + 1
+}
+
+
+// console.log(addTwo(6)) cannot access before initialization bcoz it is a expression function
+const addTwo = function (num){
+    return num + 1
+}
+console.log(addTwo(6))
