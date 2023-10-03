1852ade1948919798d95c2155d6d68323096cdf7
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-10-03 09:38:45 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("Janaury");    
+        break;
+    case 2:
+        console.log("Feb");    
+        break;
+    case 3:
+        console.log("March");    
+        break;
+    case 4:
+        console.log("April");    
+        break;
+
+    default:
+        console.log("Something wrong..!");
+        break;
+}
