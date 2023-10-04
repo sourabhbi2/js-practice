3720d1ef91d481d16fed985b5964a373bd217a8a
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-10-04 07:03:46 +0000

    learnt Nullish Coalescing & Terniary Operator

--- 04_control_flow/02_control_flow.js
+++ 04_control_flow/02_control_flow.js
@@ -32,5 +32,23 @@ const userEmail = []
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty.");
+    // console.log("Object is empty.");
 }
+
+//Nullish Coalescing Operator (??): null undefined
+/* It is a logical operator that returns its right-hand side operand when its left-hand side 
+operand is null and undefined and otherwise return left-hand side operand.
+*/
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = null ?? undefined
+console.log(val1); 
+
+// Terniary operator
+
+// condition ? true : false
+
+const teaPrice = 100
+teaPrice >= 80 ? console.log("less than 80") : console.log("More than 80");
