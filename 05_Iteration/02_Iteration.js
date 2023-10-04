d4a1351b24b04633c718ab4e64a971cdc35a976f
Sourabh Kumar <sourabhkumar1109@gmail.com>
2023-10-04 08:14:11 +0000

    while & do_while

---
+++ 05_Iteration/02_Iteration.js
@@ -0,0 +1,22 @@
+//while loop
+
+let i = 0
+while (i <= 10) {
+    // console.log(`Value of index is ${i}`);
+    i = i + 2    
+}
+
+let myArr = ["Flash", "Thor", "Batman"]
+
+let arr = 0
+while(arr < myArr.length){
+    // console.log(`Value of array is ${myArr[arr]}`);
+    arr++
+}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
